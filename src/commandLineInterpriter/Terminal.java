b17bcdfeb987f4a27337e0004cd5dd93b79f8531
Hesham97 <hesham_magdy97@hotmail.com>
2017-10-03 21:59:12 +0200

    Add some varibles ..

--- src/commandLineInterpriter/Terminal.java
+++ src/commandLineInterpriter/Terminal.java
@@ -1,16 +1,26 @@
 package commandLineInterpriter;
 
-import org.omg.PortableInterceptor.USER_EXCEPTION;
+import java.io.File;
+
 
 public class Terminal {
 	
 	private static final String  DEFULT_PATH=System.getProperty("user.dir");
 	private String currentPath;
+	
 	/*@TO-DO we must add the commands here as it 
 	 * defined as final 
 	private static final Command commands[];
 	*/
 	
+	void ls(){
+		File file = new File(currentPath);
+		File[] dirList = file.listFiles();
+		for(int i = 0 ; i<dirList.length;i++){
+			System.out.println(dirList[i].getName());
+			
+		}
+	}
 	
 	
 	
