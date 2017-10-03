9d278053c0274e5141eb5a6d48b0e3a4272be5e3
Hesham97 <hesham_magdy97@hotmail.com>
2017-10-03 20:45:14 +0200

    Add some varibles ...

--- src/commandLineInterpriter/Command.java
+++ src/commandLineInterpriter/Command.java
@@ -1,5 +1,22 @@
 package commandLineInterpriter;
 
+import java.util.Map;
+
 public class Command {
+	
+	private String name;
+	private String description;
+	private Map arguments;
+	
+	public Command(String name,String description,Map arguments){
+		
+		this.name = name;
+		this.description = description;
+		this.arguments= arguments;
+		
+	}
+	
+
+	
 
 }
--- src/commandLineInterpriter/Terminal.java
+++ src/commandLineInterpriter/Terminal.java
@@ -1,5 +1,18 @@
 package commandLineInterpriter;
 
+import org.omg.PortableInterceptor.USER_EXCEPTION;
+
 public class Terminal {
+	
+	private static final String  DEFULT_PATH=System.getProperty("user.dir");
+	private String currentPath;
+	/*@TO-DO we must add the commands here as it 
+	 * defined as final 
+	private static final Command commands[];
+	*/
+	
+	
+	
+	
 
 }
