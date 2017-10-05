6a6b07fd226e143d9b0e720d08953af7b47f5ab1
Hesham97 <hesham_magdy97@hotmail.com>
2017-10-05 19:45:20 +0200

    add main frame and text area

--- src/commandLineInterpriter/Gui.java
+++ src/commandLineInterpriter/Gui.java
@@ -1,5 +1,30 @@
 package commandLineInterpriter;
 
-public class Gui {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.TextArea;
+
+import javax.swing.JFrame;
+
+public class Gui  {
+
+	public JFrame mainFrame;
+	public TextArea textArea;
+	Terminal terminal;
+	
+	public Gui() {
+		terminal = new Terminal();
+		mainFrame = new JFrame();
+		mainFrame.setSize(600,350);
+	    textArea=new TextArea();
+		textArea.setBackground(Color.black);
+		textArea.setForeground(Color.WHITE);
+		textArea.setFont(new Font("Serif", Font.ITALIC, 19));
+		mainFrame.add(textArea);
+		mainFrame.setVisible(true);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		textArea.setText(terminal.getCurrentPath());
+		textArea.setCaretPosition(textArea.getParent().getWidth());
+	}
 
 }
